11ed898863cb17d8184776e7c0ed20daf80ef268
minguh <aiguh97@gmail.com>
2026-01-14 07:37:21 +0700

    add locale jpn

--- src/constants/data/services.js
+++ src/constants/data/services.js
@@ -5,10 +5,12 @@ export const ServicesData = [
     title: {
       en: "Web Development",
       id: "Pengembangan Web",
+      jp: "ウェブ開発",
     },
     description: {
       en: "I create dynamic, responsive websites that work seamlessly and perform at their best.",
       id: "Saya membuat situs web dinamis dan responsif yang berfungsi dengan mulus dan memiliki performa terbaik.",
+      jp: "私は、シームレスに動作し、最高のパフォーマンスを発揮する動的でレスポンシブなウェブサイトを作成します。",
     }
   },
   {
@@ -17,10 +19,12 @@ export const ServicesData = [
     title: {
       en: "UI/UX Design",
       id: "Desain UI/UX",
+      jp: "UI/UXデザイン",
     },
     description: {
       en: "I design stunning, intuitive interfaces that captivate users and elevate their experience.",
       id: "Saya mendesain antarmuka yang memukau dan intuitif, meningkatkan pengalaman pengguna secara maksimal.",
+      jp: "ユーザーを魅了し、体験を向上させる、美しく直感的なインターフェースをデザインします。",
     }
   },
   {
@@ -29,10 +33,12 @@ export const ServicesData = [
     title: {
       en: "Digital Marketing",
       id: "Pemasaran Digital",
+      jp: "デジタルマーケティング",
     },
     description: {
       en: "I craft effective strategies to improve your online presence and drive measurable results.",
       id: "Saya merancang strategi efektif untuk meningkatkan kehadiran online dan hasil yang terukur jelas.",
+      jp: "オンラインプレゼンスを向上させ、測定可能な成果を生み出す効果的な戦略を構築します。",
     }
   },
   {
@@ -41,10 +47,12 @@ export const ServicesData = [
     title: {
       en: "Consulting",
       id: "Konsultasi",
+      jp: "コンサルティング",
     },
     description: {
       en: "Let’s discuss your unique needs and find the perfect solution tailored just for you.",
       id: "Mari kita diskusikan kebutuhan khusus Anda dan temukan solusi terbaik yang saya dirancang untuk Anda.",
+      jp: "あなたのユニークなニーズについて話し合い、あなた専用に最適化された解決策を見つけましょう。",
     }
   },
 ];
--- src/pages/services.js
+++ src/pages/services.js
@@ -2,49 +2,46 @@ import { useRouter } from 'next/router';
 import { useTranslations } from 'next-intl';
 import { NextSeo } from 'next-seo';
 
-import Container from '@/components/layouts/partials/Container'
+import Container from '@/components/layouts/partials/Container';
 import FAQSection from '@/components/views/service/FAQSection';
 import PageHeading from '@/components/common/PageHeading';
-import Services from '@/components/views/service/Services'
+import Services from '@/components/views/service/Services';
 
-const ServicesPage = () => {
+const ServicesPage = ({ servicesData }) => {
+  const t = useTranslations('Services');
+  const { locale, pathname } = useRouter();
+  const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
-    const t = useTranslations('Services');
-    const { locale, pathname } = useRouter();
-    const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
-    const lang = locale == 'en' ? '/en' : ''
-    const currentPageURL = `${SITE_URL}${lang}${pathname}`
+  const langPrefix = locale === 'en' ? '/en' : locale === 'jp' ? '/jp' : '';
+  const currentPageURL = `${SITE_URL}${langPrefix}${pathname}`;
 
-    return (
-        <>
-            <NextSeo
-                title={`${t('title')} - Teguh Muhammad Harits`}
-                description={t('metaDesc')}
-                additionalLinkTags={[
-                    { rel: 'alternate', hreflang: 'x-default', href: `${SITE_URL}${pathname}` },
-                    { rel: 'alternate', hreflang: 'id', href: `${SITE_URL}${pathname}` },
-                    { rel: 'alternate', hreflang: 'en', href: `${SITE_URL}/en${pathname}` },
-                ]}
-                canonical={currentPageURL}
-                openGraph={{
-                    url: currentPageURL,
-                }}
-            />
-            <Container data-aos='fade-up'>
-                <PageHeading
-                    title={t('title')}
-                    description={t('subtitle')}
-                />
-                <div className='flex flex-col gap-6'>
-                    <Services />
-                    <FAQSection locale={locale} />
-                </div>
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <NextSeo
+        title={`${t('title')} - Teguh Muhammad Harits`}
+        description={t('metaDesc')}
+        additionalLinkTags={[
+          { rel: 'alternate', hreflang: 'x-default', href: `${SITE_URL}${pathname}` },
+          { rel: 'alternate', hreflang: 'id', href: `${SITE_URL}${pathname}` },
+          { rel: 'alternate', hreflang: 'en', href: `${SITE_URL}/en${pathname}` },
+          { rel: 'alternate', hreflang: 'jp', href: `${SITE_URL}/jp${pathname}` },
+        ]}
+        canonical={currentPageURL}
+        openGraph={{ url: currentPageURL }}
+      />
+      <Container data-aos="fade-up">
+        <PageHeading title={t('title')} description={t('subtitle')} />
+        <div className="flex flex-col gap-6">
+          <Services services={servicesData} />
+          <FAQSection locale={locale} />
+        </div>
+      </Container>
+    </>
+  );
+};
 
-export default ServicesPage
+export default ServicesPage;
+
 export const getStaticProps = async () => {
 
     return {
